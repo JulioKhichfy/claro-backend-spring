a4ef7391395012d2653d7945bee22a2a27ccb4b8
julioKhichfy <juliocesark@gmail.com>
2019-03-30 13:44:11 -0300

    Projeto criado

---
+++ src/main/java/com/claro/julio/ClaroBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.claro.julio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaroBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaroBackendSpringApplication.class, args);
+	}
+
+}
